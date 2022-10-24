3d7ca5f9ec95516881ac85211ed68a5f39493882
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-10-24 21:37:31 +0100

    Time: 47 ms (80.18%), Space: 13.9 MB (76.20%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,16 +1,26 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        ans = {}
-        for c in s:
-            if c not in ans:
-                ans[c] = 0
-            ans[c] += 1
+        #sol 1. with extra space
+#         ans = {}
+#         for c in s:
+#             if c not in ans:
+#                 ans[c] = 0
+#             ans[c] += 1
         
-        for c in t:
-            if c not in ans:
-                return c
-            ans[c] -= 1
-            if ans[c] == 0:
-                del ans[c]
-                
-        return ""
+#         for c in t:
+#             if c not in ans:
+#                 return c
+#             ans[c] -= 1
+#             if ans[c] == 0:
+#                 del ans[c]
+#         return ""
+
+
+        s = sorted(s)
+        t = sorted(t)
+        
+        for i in range(len(t) - 1):
+            if s[i] != t[i]:
+                return t[i]
+            
+        return t[-1]
