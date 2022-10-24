3ff0b24ca6eb27c08b3bc84b0b921e359219f40d
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-10-24 21:41:55 +0100

    Time: 69 ms (42.08%), Space: 14 MB (32.63%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,28 +1,33 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         #sol 1. with extra space
-#         ans = {}
-#         for c in s:
-#             if c not in ans:
-#                 ans[c] = 0
-#             ans[c] += 1
-        
-#         for c in t:
-#             if c not in ans:
-#                 return c
-#             ans[c] -= 1
-#             if ans[c] == 0:
-#                 del ans[c]
-#         return ""
+        # ans = {}
+        # for c in s:
+        #     if c not in ans:
+        #         ans[c] = 0
+        #     ans[c] += 1
+        # for c in t:
+        #     if c not in ans:
+        #         return c
+        #     ans[c] -= 1
+        #     if ans[c] == 0:
+        #         del ans[c]
+        # return ""
 
-        #sol 2. with sorting
-        s = sorted(s)
-        t = sorted(t)
+        # #sol 2. with sorting
+        # s = sorted(s)
+        # t = sorted(t)
+        # # check for diff character in "s"
+        # for i in range(len(t) - 1):
+        #     if s[i] != t[i]:
+        #         return t[i]            
+        # # otherwise it must be the last char in "t"
+        # return t[-1]
         
-        # check for diff character in "s"
-        for i in range(len(t) - 1):
-            if s[i] != t[i]:
-                return t[i]
-            
-        # otherwise it must be the last char in "t"
-        return t[-1]
+        # sol3. with xor
+        ans = 0
+        for c in s:
+            ans ^= ord(c)
+        for c in t:
+            ans ^= ord(c)
+        return chr(ans)
