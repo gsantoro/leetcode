fd0ef3fa3130ec996c3d8441ff76790442ec3f22
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-10-24 21:33:46 +0100

    Time: 76 ms (26.53%), Space: 13.9 MB (76.20%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans = {}
+        for c in s:
+            if c not in ans:
+                ans[c] = 0
+            ans[c] += 1
+        
+        for c in t:
+            if c not in ans:
+                return c
+            ans[c] -= 1
+            if ans[c] == 0:
+                del ans[c]
+                
+        return list(ans.keys())[0]
