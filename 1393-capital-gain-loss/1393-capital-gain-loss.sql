129385d77e7ff8a6e1379da4e6711af3291b42b9
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-10-26 18:36:39 +0100

    Time: 1008 ms (14.92%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,21 +1,31 @@
-with joins as (     
-	SELECT 
-	  stock_name, 
-	  operation,
-	  LEAD (price) over (partition by stock_name order by operation_day) - price as gain
-	from 
-	  Stocks),
-buys as (	 
-	select
-	  *
-	FROM 
-	  joins
-	WHERE 
-	  operation = "Buy")
-SELECT	
-  stock_name,
-  sum(gain) as capital_gain_loss
-FROM 
-  buys
-group by
+# # 1. matching sell with buy
+# with joins as (     
+# 	SELECT 
+# 	  stock_name, 
+# 	  operation,
+# 	  LEAD (price) over (partition by stock_name order by operation_day) - price as gain
+# 	from 
+# 	  Stocks),
+# buys as (	 
+# 	select
+# 	  *
+# 	FROM 
+# 	  joins
+# 	WHERE 
+# 	  operation = "Buy")
+# SELECT	
+#   stock_name,
+#   sum(gain) as capital_gain_loss
+# FROM 
+#   buys
+# group by
+#   stock_name
+
+
+select 
+  stock_name, 
+  sum(case when operation='Sell' then price else 0 end) - sum(case when operation='Buy' then price else 0 end) as capital_gain_loss
+from 
+  Stocks
+group by 
   stock_name
