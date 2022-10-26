10e4faf4860c9b4d4a34bc88f9bd4066669d7d07
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-10-26 20:11:27 +0100

    Time: 2404 ms (7.70%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,17 +1,33 @@
-SELECT distinct
+# # 1. using a nested query
+# SELECT distinct
+#   p.product_id,
+#   p.product_name
+# FROM 
+#   Sales s
+# JOIN	
+#   Product p
+# on
+#   s.product_id = p.product_id
+# WHERE 
+#   s.sale_date between '2019-01-01' and '2019-03-31' and s.product_id not in (
+# 	SELECT 
+# 	  s2.product_id
+# 	FROM 
+# 	  Sales s2
+# 	WHERE 
+# 	  s2.sale_date not between '2019-01-01' and '2019-03-31')
+      
+      
+# 2. Using having min and max
+select 
   p.product_id,
   p.product_name
 FROM 
   Sales s
-JOIN	
+JOIN 
   Product p
-on
-  s.product_id = p.product_id
-WHERE 
-  s.sale_date between '2019-01-01' and '2019-03-31' and s.product_id not in (
-	SELECT 
-	  s2.product_id
-	FROM 
-	  Sales s2
-	WHERE 
-	  s2.sale_date not between '2019-01-01' and '2019-03-31')
+on s.product_id = p.product_id
+group by 
+  1, 2
+having
+  min(sale_date) >= "2019-01-01" and max(sale_date) <= "2019-03-31"
