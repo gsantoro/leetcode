013a8912663b5909c309c8d53a6c003cab17c6e4
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-11-22 21:11:49 +0000

    Time: 1248 ms (7.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+select
+    employee_id,
+    min(department_id) as department_id
+from
+    Employee e
+group by
+    employee_id
+having
+    count(department_id) = 1
+union
+select
+    employee_id,
+    department_id
+from
+    Employee e  
+where
+    primary_flag = 'Y'
